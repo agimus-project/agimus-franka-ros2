a2ed660f6013ceedc1409ab95106dc1bee9c6b2b
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-03-12 00:41:10 +0100

    clean

--- flake.nix
+++ flake.nix
@@ -23,107 +23,84 @@
               agimus-franka-bringup = _final: _ros-final: {
                 src = lib.fileset.toSource {
                   root = ./.;
-                  fileset = lib.fileset.unions [ ./agimus_franka_bringup ];
+                  fileset = ./agimus_franka_bringup;
                 };
               };
 
               agimus-franka-example-controllers = final: _ros-final: {
                 src = lib.fileset.toSource {
                   root = ./.;
-                  fileset = lib.fileset.unions [ ./agimus_franka_example_controllers ];
+                  fileset = ./agimus_franka_example_controllers;
                 };
-                preConfigure = ''
-                  export NIX_CFLAGS_COMPILE=$(echo $NIX_CFLAGS_COMPILE | tr ' ' '\n' | grep -v '/nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' | tr '\n' ' ')
-                '';
-                cmakeFlags = [
-                  # TODO: ?
-                  "-DCMAKE_CTEST_ARGUMENTS=--exclude-regex;test_load_agimus_franka_robot_state_broadcaster"
-                ];
               };
 
               agimus-franka-fr3-moveit-config = _final: _ros-final: {
                 src = lib.fileset.toSource {
                   root = ./.;
-                  fileset = lib.fileset.unions [ ./agimus_franka_fr3_moveit_config ];
+                  fileset = ./agimus_franka_fr3_moveit_config;
                 };
               };
 
               agimus-franka-gazebo-bringup = _final: _ros-final: {
                 src = lib.fileset.toSource {
                   root = ./.;
-                  fileset = lib.fileset.unions [ ./agimus_franka_gazebo/agimus_franka_gazebo_bringup ];
+                  fileset = ./agimus_franka_gazebo/agimus_franka_gazebo_bringup;
                 };
               };
 
               agimus-franka-ign-ros2-control = _final: _ros-final: {
                 src = lib.fileset.toSource {
                   root = ./.;
-                  fileset = lib.fileset.unions [ ./agimus_franka_gazebo/agimus_franka_ign_ros2_control ];
+                  fileset = ./agimus_franka_gazebo/agimus_franka_ign_ros2_control;
                 };
               };
 
               agimus-franka-gripper = _final: _ros-final: {
                 src = lib.fileset.toSource {
                   root = ./.;
-                  fileset = lib.fileset.unions [ ./agimus_franka_gripper ];
+                  fileset = ./agimus_franka_gripper;
                 };
               };
 
-              agimus-franka-hardware =
-                final: ros-final:
-                (super: {
-                  src = lib.fileset.toSource {
-                    root = ./.;
-                    fileset = lib.fileset.unions [ ./agimus_franka_hardware ];
-                  };
-                  preConfigure = ''
-                    export NIX_CFLAGS_COMPILE=$(echo $NIX_CFLAGS_COMPILE | tr ' ' '\n' | grep -v '/nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' | tr '\n' ' ')
-                  '';
-                });
+              agimus-franka-hardware = final: ros-final: {
+                src = lib.fileset.toSource {
+                  root = ./.;
+                  fileset = ./agimus_franka_hardware;
+                };
+              };
 
               agimus-franka-msgs = _final: _ros-final: {
                 src = lib.fileset.toSource {
                   root = ./.;
-                  fileset = lib.fileset.unions [ ./agimus_franka_msgs ];
+                  fileset = ./agimus_franka_msgs;
                 };
               };
 
               agimus-franka-robot-state-broadcaster = final: ros-final: {
                 src = lib.fileset.toSource {
                   root = ./.;
-                  fileset = lib.fileset.unions [ ./agimus_franka_robot_state_broadcaster ];
+                  fileset = ./agimus_franka_robot_state_broadcaster;
                 };
-                cmakeFlags = [
-                  # TODO
-                  "-DCMAKE_CTEST_ARGUMENTS=--exclude-regex;test_load_agimus_franka_robot_state_broadcaster"
-                ];
-                preConfigure = ''
-                  export NIX_CFLAGS_COMPILE=$(echo $NIX_CFLAGS_COMPILE | tr ' ' '\n' | grep -v '/nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' | tr '\n' ' ')
-                '';
               };
 
               agimus-franka-ros2 = _final: _ros-final: {
                 src = lib.fileset.toSource {
                   root = ./.;
-                  fileset = lib.fileset.unions [ ./agimus_franka_ros2 ];
+                  fileset = ./agimus_franka_ros2;
                 };
               };
 
               agimus-franka-semantic-components = _final: _ros-final: {
                 src = lib.fileset.toSource {
                   root = ./.;
-                  fileset = lib.fileset.unions [ ./agimus_franka_semantic_components ];
+                  fileset = ./agimus_franka_semantic_components;
                 };
-                checkTarget = " ";
-                doInstallCheck = true;
-                preInstallCheck = "export AMENT_PREFIX_PATH=$out:$AMENT_PREFIX_PATH";
-                intsallCheckTarget = "test";
               };
 
               agimus-integration-launch-testing = _final: _ros-final: {
                 src = lib.fileset.toSource {
                   root = ./.;
-                  fileset = lib.fileset.unions [ ./agimus_integration_launch_testing ];
+                  fileset = ./agimus_integration_launch_testing;
                 };
               };
             };
