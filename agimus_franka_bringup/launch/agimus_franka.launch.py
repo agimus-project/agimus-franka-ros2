cd025ab6d8bf1171e24b872b3362cd8b5118dc97
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-05-03 00:14:45 +0200

    thanks ruff

--- agimus_franka_bringup/launch/agimus_franka.launch.py
+++ agimus_franka_bringup/launch/agimus_franka.launch.py
@@ -81,7 +81,7 @@ def robot_description_dependent_nodes_spawner(
             package="controller_manager",
             executable="ros2_control_node",
             parameters=[
-                franka_controllers,
+                agimus_franka_controllers,
                 {"robot_description": robot_description},
                 {"arm_id": arm_id},
                 {"load_gripper": load_gripper},
